daae42d2a29d4b622fcf815a8b9cde26271d0551
artem_ltv <litwinenkoartem1@gmail.com>
2025-03-17 22:22:29 +0300

    feat: добавлен слой BookStore.Core и модель Book

--- BookStore/BookStore.Core/obj/Debug/net8.0/BookStore.Core.AssemblyInfo.cs
+++ BookStore/BookStore.Core/obj/Debug/net8.0/BookStore.Core.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837cd09c94b1291d4adef293f9ff9bcecbf16d4e")]
-[assembly: System.Reflection.AssemblyProductAttribute("BookStore.API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("BookStore.API")]
+[assembly: System.Reflection.AssemblyProductAttribute("BookStore.Core")]
+[assembly: System.Reflection.AssemblyTitleAttribute("BookStore.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
