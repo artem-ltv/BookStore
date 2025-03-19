e612ce8decae8908b605a17c92fc0c8a578fa60e
artem_ltv <litwinenkoartem1@gmail.com>
2025-03-19 22:57:33 +0300

    feat: добавлены методы POST, PUT, DELETE в контроллер

--- BookStore/BookStore/Controllers/BooksController.cs
+++ BookStore/BookStore/Controllers/BooksController.cs
@@ -1,5 +1,6 @@
 ﻿using BookStore.API.Contracts;
 using BookStore.Core.Abstractions;
+using BookStore.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.API.Controllers
@@ -17,6 +18,45 @@ namespace BookStore.API.Controllers
             var response = books.Select(b => new BooksResponse(b.Id, b.Title, b.Description, b.Price));
 
             return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateBook([FromBody] BooksRequest request)
+        {
+            var (book, error) = Book.Create(
+                Guid.NewGuid(), 
+                request.Title, 
+                request.Description, 
+                request.Price);
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                return BadRequest(error);
+            }
+
+            var bookId = await _booksService.CreateBook(book);
+
+            return Ok(bookId);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<Guid>> UpdateBook(Guid id, [FromBody] BooksRequest request)
+        {
+            var bookId = await _booksService.UpdateBook(
+                        id, 
+                        request.Title, 
+                        request.Description, 
+                        request.Price);
+
+            return Ok(bookId);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<Guid>> DeleteBook(Guid id)
+        {
+            var bookId = await _booksService.DeleteBook(id);
+
+            return Ok(bookId);
         }
     }
 }
