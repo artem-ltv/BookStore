60d1bdb714c5189d62d912f2c83ad7db41b1c5b7
artem_ltv <litwinenkoartem1@gmail.com>
2025-05-02 23:00:39 +0300

    feat(frontend): добавлен useEffect на получение книг

--- Frontend/bookstore/app/books/page.tsx
+++ Frontend/bookstore/app/books/page.tsx
@@ -1,5 +1,29 @@
 "use client";
 
+import Button from "antd/es/button/button";
+import { useEffect, useState } from "react";
+import { Books } from "../components/Books";
+import { getAllBooks } from "../services/books";
+
 export default function BooksPage(){
-    
+    const [books, setBooks] = useState<Book[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const getBooks = async () => {
+            const books = await getAllBooks();
+            setLoading(false);
+            setBooks(books);
+        };
+
+        getBooks();
+    }, [])
+
+    return (
+        <div>
+            <Button>Добавить книгу</Button>
+
+            <Books books={books} />
+        </div>
+    )
 }
